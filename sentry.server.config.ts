2f4ed64c2fc028b260802ab7da5e5bc9d55a6710
studentwritersng <studentwritersng@gmail.com>
2026-08-04 23:36:30 -0700

    chore: add Sentry error tracking scaffolding
    Client/server/edge configs, instrumentation hooks, global error boundary and
    example routes; ignore .env.sentry-build-plugin.

---
+++ sentry.server.config.ts
@@ -0,0 +1,22 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://59fc81a2d231cea9c23401c5141fd590@o4511854930690048.ingest.de.sentry.io/4511854944911440",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
