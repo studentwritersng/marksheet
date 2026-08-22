646c5d5adf813a6f9f24dc4d2eab196290db9dae
studentwritersng <studentwritersng@gmail.com>
2026-08-22 10:17:08 +0100

    fix(mobile): allow mixed content, add push presentation opts, ignore keystores

--- mobile-app/capacitor.config.ts
+++ mobile-app/capacitor.config.ts
@@ -11,7 +11,12 @@ const config: CapacitorConfig = {
   },
   android: {
     backgroundColor: APP_CONFIG.backgroundColor,
-    allowMixedContent: false,
+    allowMixedContent: true,
+  },
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
   },
 };
 
